918bf7ad0a4c75de6084ecfc77441cc8ba63dbd2
omaddihamza <hamza.omaddi@gmail.com>
2026-01-13 14:29:17 +0100

    first commit

---
+++ src/main/java/ma/mm/automatisation_processus_achat/AutomatisationProcessusAchatApplication.java
@@ -0,0 +1,13 @@
+package ma.mm.automatisation_processus_achat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatisationProcessusAchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatisationProcessusAchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/mm/automatisation_processus_achat/AutomatisationProcessusAchatApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mm.automatisation_processus_achat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatisationProcessusAchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
